d32c344d4cc069940718e6a4faebd10caa21a533
Desom <gertjan.franken@gmail.com>
2014-03-03 23:25:56 +0100

    Toevoegen van commentaar en oplossen van TODO's

--- src/Main/AssemblyTask.java
+++ src/Main/AssemblyTask.java
@@ -1,39 +1,78 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class AssemblyTask {
 
-	private String[] actions;
+	private ArrayList<String> actions;
 	private String type;
 	private boolean isCompleted;
 
-	// TODO string array naar arraylist of linked
-	public AssemblyTask(String[] actions, String type) {
+	/**
+	 * Constructor of AssemblyTask.
+	 * Creates an assembly task with a list of actions that are needed to complete this assembly task and the type of this assembly task.
+	 * 
+	 * @param	actions
+	 * 			A list of actions that is needed to complete this AssemblyTask.
+	 * @param	type
+	 * 			A string that indicates the type of this AssemblyTask.
+	 */
+	public AssemblyTask(ArrayList<String> actions, String type) {
 		this.setActions(actions);
 		this.setType(type);
 		isCompleted = false;
 	}
 	
+	/**
+	 * Gives the type of this assembly task.
+	 * 
+	 * @return	The type of this AssemblyTask.
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Sets the type of this assembly task. Only used in constructor.
+	 * 
+	 * @param	type
+	 * 			The string that indicates the type of this AssemblyTask.
+	 */
 	private void setType(String type) {
 		this.type = type;
 	}
 	
-	// public want strings vast datatype
-	public String[] getActions() {
-		return actions.clone();
+	/**
+	 * Gives a list of the actions needed to complete this assembly task.
+	 * 
+	 * @return	A copy of the list of all actions needed to complete this AssemblyTask.
+	 */
+	public ArrayList<String> getActions() {
+		return (ArrayList<String>) actions.clone();
 	}
 
-	private void setActions(String[] actions) {
+	/**
+	 * Sets the needed actions to complete this assembly task. Only used in constructor.
+	 * 
+	 * @param	actions
+	 * 			The list of actions needed to complete this AssemblyTask.
+	 */
+	private void setActions(ArrayList<String> actions) {
 		this.actions = actions;
 	}
 	
+	/**
+	 * Returns true if this assembly task is completed, otherwise false.
+	 * 
+	 * @return	True if this AssemblyTask is completed, otherwise false.
+	 */
 	public boolean isCompleted() {
 		return this.isCompleted;
 	}
 	
+	/**
+	 * Completes this assembly task.
+	 */
 	public void completeTask() {
 		this.isCompleted = true;
 	}
--- src/Main/Workstation.java
+++ src/Main/Workstation.java
@@ -1,46 +1,75 @@
 package Main;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
+// TODO Nieuwe error-types gebruiken als ze worden gepusht
 
 public class Workstation {
 
 	private int id;
-	private ArrayList<String> taskTypes;
+	private final ArrayList<String> taskTypes;
 	private User carMechanic;
 	private ArrayList<AssemblyTask> allTasks;
 	private AssemblyTask activeTask;
 
+	/**
+	 * Constructor of Workstation.
+	 * Creates a new workstation with a specific id and a list of taskTypes.
+	 * 
+	 * @param id
+	 * @param taskTypes
+	 */
 	public Workstation(int id, ArrayList<String> taskTypes) {
 		this.setId(id);
-		this.setTaskTypes(taskTypes);
+		this.taskTypes = taskTypes;
 		this.allTasks = new ArrayList<AssemblyTask>();
 		this.activeTask = null;
 	}
 	
+	/**
+	 * Getter for the id of this workstation.
+	 * 
+	 * @return The id of this Workstation.
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id of this workstation. Only used in constructor.
+	 * 
+	 * @param	id
+	 * 			The id that is given to this Workstation.
+	 */
 	private void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Gives a list of all task types this workstation can perform.
+	 * 
+	 * @return A copy of all possible types of the tasks this Workstation can perform.
+	 */
 	public ArrayList<String> getTaskTypes() {
 		return (ArrayList<String>) taskTypes.clone();
 	}
-
-	// niet public, final
-	public void setTaskTypes(ArrayList<String> taskTypes) {
-		this.taskTypes = taskTypes;
-	}
 	
+	/**
+	 * Gives the car mechanic that is operating at this workstation.
+	 * 
+	 * @return The CarMechanic operating at this Workstation.
+	 */
 	public User getCarMechanic() {
 		return carMechanic;
 	}
 
-	// hernoemen naar set / assign?
+	/**
+	 * Add a given car mechanic to this workstation.
+	 * 
+	 * @param	carMechanic
+	 * 			The desired car mechanic to operate at this Workstation.
+	 * @throws 	Exception
+	 * 			If there is already a car mechanic operating at this Workstation.
+	 */
 	public void addCarMechanic(User carMechanic) throws Exception {
 		if (this.carMechanic == null)
 			this.carMechanic = carMechanic;
@@ -48,10 +77,19 @@ public class Workstation {
 			throw new Exception("There already has been assigned a car mechanic to this workstation");
 	}
 	
-	// Wie voert deze methode uit?
-	// bij het creeeren
-	// rename
-	public void match(User user, AssemblyTask task) throws Exception {
+	// TODO welke user is authorized?
+	/**
+	 * Adds an assembly task to this workstation.
+	 * 
+	 * @param	user
+	 * 			The User that wants to call this method.
+	 * @param	task
+	 * 			The AssemblyTask that will be added.
+	 * @throws	Exception
+	 * 			If task does not match with the task types list.
+	 * 			If the user is not authorized to call the given method.
+	 */
+	public void addAssemblyTask(User user, AssemblyTask task) throws Exception {
 		this.checkUser(user, "match");
 		if (this.taskTypes.contains(task.getType()))
 			this.allTasks.add(task);
@@ -59,20 +97,48 @@ public class Workstation {
 			throw new Exception("This assembly task can't be assigned to this workstation");
 	}
 	
+	/**
+	 * Selects the given task to be the active task of this workstation.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @param	task
+	 * 			The task that the user wants to work on.
+	 * @throws	Exception
+	 * 			If another task is still in progress.
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public void selectTask(User user, AssemblyTask task) throws Exception {
 		this.checkUser(user, "selectTask");
 		if (this.activeTask == null)
 			this.activeTask = task;
 		else
-			throw new Exception("Another assemly task is still in progress");
+			throw new Exception("Another assembly task is still in progress");
 	}
 	
+	/**
+	 * Completes the assembly task that the operating car mechanic is working on.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @throws	Exception
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public void completeTask(User user) throws Exception {
 		this.checkUser(user, "completeTask");
 		this.activeTask.completeTask();
 		this.activeTask = null;
 	}
 	
+	/**
+	 * Gives a list of all pending assembly tasks.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @return	A copy of the list of all pending assembly tasks.
+	 * @throws	Exception
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public ArrayList<AssemblyTask> getAllPendingTasks(User user) throws Exception {
 		this.checkUser(user, "getAllPendingTasks");
 		ArrayList<AssemblyTask> allPendingTasks = new ArrayList<AssemblyTask>();
@@ -82,6 +148,15 @@ public class Workstation {
 		return (ArrayList<AssemblyTask>) allPendingTasks.clone();
 	}
 	
+	/**
+	 * Gives a list of all completed assembly tasks.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @return	A copy of the list of all completed assembly tasks.
+	 * @throws Exception
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public ArrayList<AssemblyTask> getAllCompletedTasks(User user) throws Exception {
 		this.checkUser(user, "getAllCompletedTasks");
 		ArrayList<AssemblyTask> allCompletedTasks = new ArrayList<AssemblyTask>();
@@ -91,11 +166,29 @@ public class Workstation {
 		return (ArrayList<AssemblyTask>) allCompletedTasks.clone();
 	}
 	
+	/**
+	 * Gives a list of all assembly tasks.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @return	A copy of the list of all assembly tasks.
+	 * @throws	Exception
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public ArrayList<AssemblyTask> getAllTasks(User user) throws Exception {
 		this.checkUser(user, "getAllTasks");
 		return (ArrayList<AssemblyTask>) this.allTasks.clone();
 	}
 	
+	/**
+	 * Returns true if all assembly tasks of this workstation are completed, otherwise false.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @return	True if all assembly tasks are completed, otherwise false.
+	 * @throws	Exception
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public boolean hasAllTasksCompleted(User user) throws Exception {
 		this.checkUser(user, "hasAllTasksCompleted");
 		for (AssemblyTask task : this.allTasks)
@@ -104,8 +197,16 @@ public class Workstation {
 		return true;
 	}
 	
-	// Niet in klasse diagram, maar nodig in use case
-	// TODO Formaat nog verbeteren
+	/**
+	 * Returns the task type of the active assembly task of this workstation along with the actions needed to complete this task.
+	 * 
+	 * @param	user
+	 * 			The user that wants to call this method.
+	 * @return	A list with on the first line the task type of the active assembly task. The following lines indicate the actions needed to complete this task.
+	 * @throws	Exception
+	 * 			If there is no active assembly task.
+	 * 			If the user is not authorized to call the given method.
+	 */
 	public ArrayList<String> getActiveTaskInformation(User user) throws Exception {
 		this.checkUser(user, "match");
 		if (this.activeTask == null)
@@ -117,8 +218,18 @@ public class Workstation {
 		return information;
 	}
 	
+	/**
+	 * Checks if the give user can perform the given method (defined by a string). 
+	 * 
+	 * @param	user
+	 * 			The user that wants to call the given method.
+	 * @param	methodString
+	 * 			The string that defines the method.
+	 * @throws	Exception
+	 * 			If the user is not authorized to call the given method.
+	 */
 	private void checkUser(User user, String methodString) throws Exception {
 		if (!user.canPerform(methodString))
-			throw new Exception("User is not authorized for this action");
+			throw new Exception("This user is not authorized for this action");
 	}
 }

d5f93d271782e0b4863db7837a623fba51407f30
polle-vanhoof <pello_ski@hotmail.com>
2014-04-07 16:56:56 +0200

    exception handling in de policies

---
+++ src/domain/user/CustomShopManager.java
@@ -0,0 +1,15 @@
+package domain.user;
+
+public class CustomShopManager extends User {
+
+	/**
+	 * Contructor of Custom Shop Manager.
+	 * 
+	 * @param	id
+	 * 			id of this custom shop manager
+	 */
+	public CustomShopManager(int id) {
+		super(id);
+	}
+
+}

ef8adcf69b7708dea110dc519f485c91a89e0b7f
Desom <gertjan.franken@gmail.com>
2014-04-24 16:11:24 +0200

    Nog iets vergeten

--- src/controller/garageHolder/OrderNewCarHandler.java
+++ src/controller/garageHolder/OrderNewCarHandler.java
@@ -3,7 +3,6 @@ package controller.garageHolder;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.LinkedList;
 import java.util.List;
 
 import controller.CommunicationTool;
